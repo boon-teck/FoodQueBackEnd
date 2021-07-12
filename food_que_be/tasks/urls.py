65c5fada6ed46457c30bfb4ab7f248c9ca020594
Boon <boonteck.soh@hotmail.com>
2021-07-12 11:28:45 +0800

    Tasks CRUD(v1) completed

--- food_que_be/tasks/urls.py
+++ food_que_be/tasks/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.all_tasks, name='tasks_main_page'),
     path('show/<uuid:id>',views.show_task, name='task_show_page'),
     path('create/',views.create_task, name='task_create_page'),
+    path('edit/<uuid:taskid>',views.edit_task, name='task_show_page'),
+    path('delete/<uuid:taskid>',views.delete_task, name='tasks_main_page'),
 ]
--- food_que_be/tasks/views.py
+++ food_que_be/tasks/views.py
@@ -3,7 +3,7 @@ from tasks.models import Task
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tasks.serializers import TaskSerializer
-
+from rest_framework import status
 
 @api_view(['GET'])
 def all_tasks(request):
@@ -23,5 +23,26 @@ def create_task(request):
 
     if serializer.is_valid():
         serializer.save()
-    
+    else: 
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data)
+    
+
+@api_view(['PUT'])
+def edit_task(request,taskid):
+    try:
+        task = Task.objects.get(pk=taskid)
+    except Task.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = TaskSerializer(task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['DELETE'])
+def delete_task(request, taskid):
+    task = Task.objects.get(pk=taskid)
+    task.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
